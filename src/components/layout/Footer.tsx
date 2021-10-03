9a2629f348591b41f59858cd3f5ef0b205a2a466
lsandoval9 <lizarraga309@gmail.com>
2021-10-03 11:15:53 -0400

    added layout components, Header and Footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            i'm a footer
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Header(props: any){
+    return (
+        <div>
+            im a header
+        </div>
+    )
+}
+
+export default Header
