eedfc15bb68bd2d86115ccde4b9fdbe08fb605b2
lsandoval9 <lizarraga309@gmail.com>
2021-10-03 15:50:30 -0400

    added mui styles and sass components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "./styles/index.scss";
 import "mdb-ui-kit/css/mdb.min.css"
+import MainRoutes from './routes/MainRoutes';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import { ThemeProvider } from '@mui/system';
+import { createTheme } from '@mui/material';
+
+
+const theme = createTheme({
+
+    
+    palette: {
+        
+        primary: {
+          main: '#d81139',
+          contrastText: "#ffffff"
+
+        },
+        secondary: {
+          main: '#663399',
+        },
+        error: {
+          main: '#ff3d00',
+        },
+        warning: {
+          main: '#ffe900',
+        },
+        info: {
+          main: '#006bbf',
+        },
+        text: {
+          primary: 'rgba(0,0,0,0.87)',
+        },
+        divider: 'rgba(0,0,0,0.16)',
+        background: {
+          default: '#f5f5f5',
+        },
+      },
+
+})
+
 
 function App() {
 
 
 
   return (
-    <div className="App">
-        <button className="btn btn-success" onClick={() => console.log("click")}>
-            hello
-        </button>
-    </div>
+    <ThemeProvider theme={theme}>
+        <MainRoutes Header={Header} Footer={Footer} />
+    </ThemeProvider>
   );
 }
 
